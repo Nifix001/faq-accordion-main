e3962c522d94900832a956dab59f9a91cecc1615
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2024-03-21 12:51:36 +0100

    update

--- myCode/src/App.tsx
+++ myCode/src/App.tsx
@@ -5,16 +5,25 @@ function App() {
 
   return (
   <div className="container">
-      <div className="top"></div>
-      <div className='whole'>
-        <Container fluid className="d-flex justify-content-center align-items-center gx-5 gy-5">
-        <Row>
-        <Col>
-          <Card className='p-2'>      
-            <Card.Header>FAQs</Card.Header>
+      <div className="whole"></div>
+      <div className='top'>
+        <Container fluid className="gx-5 gy-5">
+        <Row className='justify-content-center align-items-center vh-100'>
+        <Col md={6}>
+          <Card className='p-2 content shadow'>      
+            <Card.Header className="d-flex justify-content-left align-items-center">
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="41" fill="none" viewBox="0 0 40 41"><path fill="#AD28EB" d="M37.5 20.5a2.467 2.467 0 0 1-1.64 2.344l-9.913 3.604-3.603 9.911a2.5 2.5 0 0 1-4.688 0l-3.604-9.922-9.911-3.593a2.5 2.5 0 0 1 0-4.688l9.921-3.604 3.594-9.911a2.5 2.5 0 0 1 4.688 0l3.604 9.921 9.911 3.594A2.467 2.467 0 0 1 37.5 20.5Z"/></svg>
+              </span>
+              <h1>FAQs</h1>
+            </Card.Header>
+            <Card.Body>
               <Accordion defaultActiveKey="0">
               <Accordion.Item eventKey="0">
-                <Accordion.Header>What is Frontend Mentor, how will it help me?</Accordion.Header>
+                <Accordion.Header>
+                  What is Frontend Mentor, how will it help me?
+                  <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg></span>
+                  </Accordion.Header>
                 <Accordion.Body>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -62,6 +71,7 @@ function App() {
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
